e3cf830678d9080dc0b30c21d84b9857e0693f22
Spencer Taylor <spencertaylor808@gmail.com>
2018-04-25 20:01:23 +0000

    Create error_exit function

---
+++ util_func.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+ * error_exit - frees the stack and exits due to erro
+ * @stack: pointer to the head of the stack
+ *
+ */
+void error_exit(stackt_t **stack)
+{
+	if (stack)
+		free_dlistint(*stack);
+	exit(EXIT_FAILURE);
+}
