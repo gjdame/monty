c74a1ba0b976d2ffe744012d9e00bcc47c342f26
gjdame <gregoryjdame@gmail.com>
2018-04-25 22:47:12 +0000

    reorder null checks on pchar

--- stack_func2.c
+++ stack_func2.c
@@ -17,18 +17,21 @@ void _pchar(stack_t **stack, unsigned int line_number)
 	stack_t *runner;
 	int val;
 
+	if (*stack == NULL)
+        {
+                printf("L%d: can't pchar, stack empty\n", line_number);
+		error_exit(stack);
+        }
+
 	runner = *stack;
 	val = runner->n;
+
 	if (val < 0 || val > 127)
 	{
 		printf("L%d: can't pchar, value out of range\n", line_number);
 		error_exit(stack);
 	}
-	if (*stack == NULL)
-	{
-		printf("L%d: can't pchar, stack empty\n", line_number);
-		error_exit(stack);
-	}
+
 	putchar(val);
 	putchar('\n');
 }
