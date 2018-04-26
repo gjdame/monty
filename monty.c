6a1f427edd1adad71f65db7dcaf32c2f84861928
gjdame <gregoryjdame@gmail.com>
2018-04-26 20:08:04 +0000

    fix global betty error

--- monty.c
+++ monty.c
@@ -1,12 +1,11 @@
 #include "monty.h"
+int push_arg = 0;
 /**
  * main - driver function for monty program
  * @ac: int num of arguments
  * @av: opcode file
  * Return: 0
  */
-int push_arg = 0;
-
 int main(int ac, char **av)
 {
 	stack_t *stack;
