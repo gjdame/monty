4728fbf8d160e42d425887dcce2e812375c40033
gjdame <gregoryjdame@gmail.com>
2018-04-26 19:57:17 +0000

    betty style

--- monty.c
+++ monty.c
@@ -1,7 +1,9 @@
 #include "monty.h"
 /**
- *
- *
+ * main - driver function for monty program
+ * @ac: int num of arguments
+ * @av: opcode file
+ * Return: 0
  */
 int push_arg = 0;
 
@@ -17,5 +19,5 @@ int main(int ac, char **av)
 	}
 	read_file(av[1], &stack);
 	free_dlistint(stack);
-	return(0);
+	return (0);
 }
