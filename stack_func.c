f61b7b7a7e219576a86ab2c8255026bb5b1a7776
Spencer Taylor <spencertaylor808@gmail.com>
2018-04-26 21:55:14 +0000

    Remove unused attribute from push function

--- stack_func.c
+++ stack_func.c
@@ -4,7 +4,7 @@
  * @stack: linked lists for monty stack
  * @line_number: number of line opcode occurs on
  */
-void _push(stack_t **stack, __attribute__ ((unused))unsigned int line_number)
+void _push(stack_t **stack, unsigned int line_number)
 {
 	stack_t *new;
 	char *arg;
