f8fd395eb2c1f0e82e8c36efffc56466cc7b5901
gjdame <gregoryjdame@gmail.com>
2018-04-26 18:01:22 +0000

    error message if not enough arguments

--- monty.c
+++ monty.c
@@ -13,7 +13,7 @@ int main(int ac, char **av)
 	if (ac != 2)
 	{
 		printf("USAGE: monty file\n");
-		error_exit(stack);
+		error_exit(&stack);
 	}
 	read_file(av[1], &stack);
 	free_dlistint(stack);
